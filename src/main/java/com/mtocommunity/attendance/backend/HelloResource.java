b3394397c1837f09d0909f7dbb0ca881ed1f01df
Luis Bazán <luis@bazan.pe>
2024-11-20 18:48:08 -0500

    Change hello world to JSON response

--- src/main/java/com/mtocommunity/attendance/backend/HelloResource.java
+++ src/main/java/com/mtocommunity/attendance/backend/HelloResource.java
@@ -3,12 +3,14 @@ package com.mtocommunity.attendance.backend;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+import org.json.JSONObject;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    @Produces("application/json")
+    public Response hello() {
+        return Response.ok(new JSONObject().put("message", "Hello world from Tomcat!").toString()).build();
     }
 }
